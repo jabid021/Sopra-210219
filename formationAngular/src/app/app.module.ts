3829d525e7f61479399f4e2ba2117f9730c7c991
oliviergoz <o.gozlan@ajc-ingenierie.fr>
2021-04-22 16:09:06 +0200

    correction couleur

--- formationAngular/src/app/app.module.ts
+++ formationAngular/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { FormsModule } from '@angular/forms';
+import { CouleurComponent } from './couleur/couleur.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent],
+  declarations: [AppComponent, FirstComponent, CouleurComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
